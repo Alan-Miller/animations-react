fac8743fe7a2328f9fcf9a20ea7807369be01642
Alan-Miller <alan.ashman@gmail.com>
2017-11-06 00:00:16 -0700

    readme and minor edits

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ class App extends Component {
         <div className="container">
           <div
             className={this.state.redClicked ? "red box animate-red" : "red box"}
-            onClick={_ => this.setState({redClicked: !this.state.redClicked})}>
+            onClick={ _ => this.setState({redClicked: !this.state.redClicked})}>
           </div>
           <div
             className={this.state.blueClicked ? "blue box animate-blue" : "blue box"}
-            onClick={_ => this.setState({blueClicked: !this.state.blueClicked})}>
+            onClick={ _ => this.setState({blueClicked: !this.state.blueClicked})}>
           </div>
           <p className="text">hover or click for animated effects!</p>
         </div>
